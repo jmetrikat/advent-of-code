a1a06e234fa4cf90e0af69952faa88b4e083cfd6
jmetrikat <jannis.metrikat@uni-potsdam.de>
2022-12-22 13:44:24 +0100

    Add day-04

--- day-03-v2.py
+++ day-03-v2.py
@@ -1,5 +1,4 @@
 #! /usr/bin/env python
-import string
 
 def compare(lines: list):
     global priority
---
+++ day-04-v2.py
@@ -0,0 +1,17 @@
+#! /usr/bin/env python
+import re
+
+result = 0
+with open("day-04-input.txt", "r") as f:
+    pairs = f.read().split("\n")
+
+for line in pairs:
+    if line != '':
+        l1, l2, r1, r2 = [int(x) for x in re.split('[,-]', line)]
+        elf1 = set(range(l1, l2+1))
+        elf2 = set(range(r1, r2+1))
+
+        if elf2.intersection(elf1):
+            result += 1
+
+print(result)
