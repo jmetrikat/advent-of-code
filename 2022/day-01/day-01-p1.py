ec034d5b4f5fddf776a5e08ad5258fd7dc65b744
jmetrikat <jannis.metrikat@uni-potsdam.de>
2022-12-27 21:29:48 +0100

    Add day-01

--- 2022/day-01/day-01-p1.py
+++ 2022/day-01/day-01-p1.py
@@ -1,11 +1,14 @@
 #!/usr/bin/env python
+# -*- coding: utf-8 -*-
 
-maximum = 0 
+""" --- Advent of Code 2022 - Day 1: Calorie Counting ---  """
+
+maximum = 0
 
 with open('day-01-input.txt') as f:
     summe = 0
     for line in f.readlines():
-        if line == "\n": 
+        if line == "\n":
             if summe >= maximum:
                 maximum = summe
             summe = 0
--- 2022/day-01/day-01-p2.py
+++ 2022/day-01/day-01-p2.py
@@ -1,11 +1,14 @@
 #!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+""" --- Advent of Code 2022 - Day 1: Calorie Counting ---  """
 
 blocks = []
 
 with open('day-01-input.txt') as f:
     summe = 0
     for line in f.readlines():
-        if line == "\n": 
+        if line == "\n":
             blocks.append(summe)
             summe = 0
         else:
