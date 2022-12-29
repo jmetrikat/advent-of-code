8d9c72aa292f7a59271af07dd3a4af731bed63c0
Jannis Metrikat <120120832+jmetrikat@users.noreply.github.com>
2022-12-29 09:17:55 +0100

    Add day-10

---
+++ 2022/day-10/day-10-p1.py
@@ -0,0 +1,33 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+""" --- Advent of Code 2022 - Day 10: Cathode-Ray Tube --- """
+
+# increment the cycle counter and update the score for given cycles
+def tick():
+    global no_cycles, x, score
+    if (no_cycles == 20 or no_cycles == 60 or no_cycles == 100 or no_cycles == 140 or no_cycles == 180 or no_cycles ==220):
+        score += no_cycles * x
+    no_cycles += 1
+    return
+
+
+with open("day-10-input.txt") as f:
+    input = f.readlines()
+
+# store number of cycles, value in register x and final score
+no_cycles = 1
+x = 1
+score = 0
+
+# noop takes 1 cycle, addx takes 2 cycles
+for line in input:
+    if line == "noop\n":
+        tick()
+    else:
+        tick()
+        tick()
+        instruction, value = line.split()
+        x += int(value)
+
+print(score)
---
+++ 2022/day-10/day-10-p2.py
@@ -0,0 +1,40 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+""" --- Advent of Code 2022 - Day 10: Cathode-Ray Tube --- """
+
+# increment the cycle counter and print a pixel
+def tick():
+    global no_cycles, x
+    printer_pos = no_cycles % 40
+
+    # if the x value (+1/-1) is in range of the printer, a # is printed
+    if (printer_pos == x-1 or printer_pos == x or printer_pos == x+1):
+        print('#', end="")
+    else:
+        print('.', end="")
+
+    # start a new line every 40 cycles
+    no_cycles += 1
+    if (no_cycles % 40 == 0):
+        print("")
+
+    return
+
+
+with open("day-10-input.txt") as f:
+    input = f.readlines()
+
+# store number of cycles, value in register x and final score
+no_cycles = 0
+x = 1
+
+# noop takes 1 cycle, addx takes 2 cycles
+for line in input:
+    if line == "noop\n":
+        tick()
+    else:
+        tick()
+        tick()
+        instruction, value = line.split()
+        x += int(value)
