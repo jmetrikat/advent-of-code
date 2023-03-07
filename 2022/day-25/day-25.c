5c6e97deedf7aac154818a80b56e9199c4a98966
Jannis Metrikat <120120832+jmetrikat@users.noreply.github.com>
2023-03-07 18:18:04 +0100

    Improve day-25

--- 2022/day-25/day-25.c
+++ 2022/day-25/day-25.c
@@ -12,37 +12,37 @@
 int part_1(char input_file[]) {
     FILE *fp = fopen(input_file, "r");
     char buffer[MAX_LENGTH];
-    long long decimal_res = 0;
+    long long base10_res = 0;
 
     /* file opened successfully */
     if (fp != NULL) {
         while (fgets(buffer, MAX_LENGTH, fp) != NULL) {
-            long long decimal_sum = 0;
+            long long base10_sum = 0;
 
             /* convert SNAFU to base 10 */
             for (int pow_val = strlen(buffer) - 2; pow_val >= 0; pow_val--) {
                 switch (buffer[strlen(buffer) - 2 - pow_val]) {
                     case '-':
-                        decimal_sum += pow(BASE, pow_val) * -1;
+                        base10_sum += pow(BASE, pow_val) * -1;
                         break;
                     case '=':
-                        decimal_sum += pow(BASE, pow_val) * -2;
+                        base10_sum += pow(BASE, pow_val) * -2;
                         break;
                     default:
-                        decimal_sum += pow(BASE, pow_val) * (buffer[strlen(buffer) - 2 - pow_val] - '0');
+                        base10_sum += pow(BASE, pow_val) * (buffer[strlen(buffer) - 2 - pow_val] - '0');
                 }
             }
 
-            decimal_res += decimal_sum;
+            base10_res += base10_sum;
         }
 
-        /* convert decimal result to base 5 */
+        /* convert base 10 result to base 5 */
         int base5_res[MAX_LENGTH];
         int len = 0;
 
-        for (len = 0; decimal_res > 0; len++) {
-            base5_res[len] = decimal_res % BASE;
-            decimal_res = decimal_res / BASE;
+        for (len = 0; base10_res > 0; len++) {
+            base5_res[len] = base10_res % BASE;
+            base10_res = base10_res / BASE;
         }
 
         /* adjust base 5 to SNAFU */
@@ -52,14 +52,14 @@ int part_1(char input_file[]) {
                 base5_res[i] = base5_res[i] + carry;
                 carry = 0;
             } else {
-                base5_res[i] = (base5_res[i] + carry) -5;
+                base5_res[i] = (base5_res[i] + carry) - 5;
                 carry = 1;
             }
         }
 
         /* print result in SNAFU */
         for (int i = len - 1; i >= 0; i--) {
-            switch(base5_res[i]) {
+            switch (base5_res[i]) {
                 case -1:
                     printf("-");
                     break;
@@ -74,7 +74,7 @@ int part_1(char input_file[]) {
 
     /* file not found */
     } else {
-        fprintf(stderr,"Problems opening file '%s'\n", input_file);
+        fprintf(stderr, "Problems opening file '%s'\n", input_file);
         exit(1);
     }
 
@@ -90,9 +90,9 @@ int part_2(char input_file[]) {
     if (fp != NULL) {
         printf(":)\n");
 
-    /* file not found */
+        /* file not found */
     } else {
-        fprintf(stderr,"Problems opening file '%s'\n", input_file);
+        fprintf(stderr, "Problems opening file '%s'\n", input_file);
         exit(1);
     }
 
