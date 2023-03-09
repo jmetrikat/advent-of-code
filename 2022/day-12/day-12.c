b1c11e741576e3f83eac8a567f9c6ffa58a3bc32
Jannis Metrikat <120120832+jmetrikat@users.noreply.github.com>
2023-03-09 14:25:11 +0100

    Add day-12 part 1 in C

---
+++ 2022/day-12/day-12.c
@@ -0,0 +1,164 @@
+/* --- Advent of Code 2023 - Day 12: Hill Climbing Algorithm --- */
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+#define MAX_LENGTH 256
+#define MAX_HEIGHT 100
+#define MAX_WIDTH 100
+
+/* solution part-1 */
+int part_1(char input_file[]) {
+    FILE *fp = fopen(input_file, "r");
+    char buffer[MAX_LENGTH];
+    char map[MAX_HEIGHT][MAX_WIDTH];
+
+    /* file opened successfully */
+    if (fp != NULL) {
+        int start[2], end[2];
+        int row, col;
+
+        /* generate map */
+        for (row = 0; fgets(buffer, MAX_LENGTH - 1, fp); row++) {
+            for (col = 0; (buffer[col] >= 'a' && buffer[col] <= 'z') || (buffer[col] == 'S') || (buffer[col] == 'E'); col++) {
+                map[row][col] = buffer[col];
+
+                /* find start */
+                if (buffer[col] == 'S') {
+                    start[0] = col;
+                    start[1] = row;
+                    map[row][col] = 'a';
+                }
+
+                /* find end */
+                if (buffer[col] == 'E') {
+                    end[0] = col;
+                    end[1] = row;
+                    map[row][col] = 'z';
+                }
+            }
+        }
+
+
+        /* use dijikstra algorithm */
+        int no_nodes = row * col;
+        int *distance = (int *)malloc(sizeof(int) * no_nodes);
+        int *visited = (int *)malloc(sizeof(int) * no_nodes);
+        int **cost = (int **)malloc(sizeof(int *) * no_nodes);
+
+        for (int i = 0; i < no_nodes; i++) {
+            cost[i] = (int *)malloc(sizeof(int) * no_nodes);
+        }
+
+        /* initialize distance matrix */
+        for (int src = 0; src < no_nodes; src++) {
+            for (int dest = 0; dest < no_nodes; dest++) {
+                if (src == dest) {
+                    /* distance from node to itself is 0 */
+                    cost[src][dest] = 0;
+                } else {
+                    /* distance of all other nodes is infinity */
+                    cost[src][dest] = no_nodes;
+
+                    /* distance of all adjacent and by elevation recheable nodes is 1 */
+                    int src_x = src % col;
+                    int src_y = src / col;
+                    int dest_x = dest % col;
+                    int dest_y = dest / col;
+
+                    if ((abs(src_x - dest_x) == 1 && src_y == dest_y) || (abs(src_y - dest_y) == 1 && src_x == dest_x)) {
+                        if (map[dest_y][dest_x] - map[src_y][src_x] <= 1) {
+                            cost[src][dest] = 1;
+                        }
+                    }
+                }
+            }
+        }
+
+        /* initialize distance and visited */
+        for (int i = 0; i < no_nodes; i++) {
+            distance[i] = cost[start[0] + start[1] * col][i];
+            visited[i] = 0;
+        }
+        distance[start[0] + start[1] * col] = 0;
+        visited[start[0] + start[1] * col] = 1;
+
+        /* calculate paths from start to all other nodes */
+        for (int i = 0; i < no_nodes; i++) {
+            int min_distance = no_nodes;
+            int next_node_idx = 0;
+
+            /* find node with minimum distance */
+            for (int j = 0; j < no_nodes; j++) {
+                if (visited[j] == 0 && distance[j] < min_distance) {
+                    min_distance = distance[j];
+                    next_node_idx = j;
+                }
+            }
+
+            /* mark node as visited */
+            visited[next_node_idx] = 1;
+
+            /* update distance of all adjacent nodes */
+            for (int j = 0; j < no_nodes; j++) {
+                if (visited[j] == 0 && distance[j] > min_distance + cost[next_node_idx][j]) {
+                    distance[j] = min_distance + cost[next_node_idx][j];
+                }
+            }
+        }
+
+        printf("%d\n", distance[end[0] + end[1] * col]);
+
+        /* free memory */
+        for (int i = 0; i < no_nodes; i++)
+            free(cost[i]);
+        free(cost);
+        free(visited);
+        free(distance);
+
+    /* file not found */
+    } else {
+        fprintf(stderr, "Problems opening file '%s'\n", input_file);
+        exit(1);
+    }
+
+    return 0;
+}
+
+/* solution part-2 */
+int part_2(char input_file[]) {
+    FILE *fp = fopen(input_file, "r");
+    char buffer[MAX_LENGTH];
+
+    /* file opened successfully */
+    if (fp != NULL) {
+        // TODO
+
+    /* file not found */
+    } else {
+        fprintf(stderr, "Problems opening file '%s'\n", input_file);
+        exit(1);
+    }
+
+    return 0;
+}
+
+/* main function */
+int main(int argc, char *argv[]) {
+    if (argc != 3) {
+        fprintf(stderr, "Usage: %s -p[1|2] <input-file>\n", argv[0]);
+        return 1;
+    }
+
+    if (!strcmp(argv[1], "-p1")) {
+        part_1(argv[2]);
+    } else if (!strcmp(argv[1], "-p2")) {
+        part_2(argv[2]);
+    } else {
+        fprintf(stderr, "Invalid argument: '%s'\n", argv[1]);
+        return 1;
+    }
+
+    return 0;
+}
