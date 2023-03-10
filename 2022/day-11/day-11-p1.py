56fb4493dd7be5524b7c4b5f0ba3f71092de14ed
Jannis Metrikat <120120832+jmetrikat@users.noreply.github.com>
2023-03-10 19:33:37 +0100

    Add files to archive

--- 2022/day-11/day-11-p1.py
+++
@@ -1,4 +0,0 @@
-#!/usr/bin/env python
-# -*- coding: utf-8 -*-
-
-""" --- Advent of Code 2022 - Day 11: Monkey in the Middle --- """
--- 2022/day-25/day-25-p2.py
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env python
-# -*- coding: utf-8 -*-
-
-""" --- Advent of Code 2023 - Day 25: Full of Hot Air --- """
-
-
